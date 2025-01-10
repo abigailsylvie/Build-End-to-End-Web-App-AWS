5cab3abfefa06a5de68c1b426421716c285f957b
abigailsylvie <103701514+abigailsylvie@users.noreply.github.com>
2025-01-10 18:39:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ZT82sZQs3', // e.g., us-east-1_xxxxx
-        userPoolClientId: '780i8hti5tksrq45s33c8o266m', // e.g., xxxxxxxxxx
-        clientSecret: '15vjtsgsqrumoolif83de1a3kd0ht5el75ufe7ptqo2eu89a97du', // Replace with your actual client secret
-        region: 'us-east-1' // e.g., us-east-1
+        userPoolId: 'us-east-1_ZT82sZQs3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '780i8hti5tksrq45s33c8o266m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g., https://xxxxx.execute-api.us-east-1.amazonaws.com/prod
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
