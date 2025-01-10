be2eab3a45a76c420a385eb3810121b9923a5cc8
abigailsylvie <103701514+abigailsylvie@users.noreply.github.com>
2025-01-10 18:32:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ZT82sZQs3',
-        userPoolClientId: '780i8hti5tksrq45s33c8o266m',
-        region: 'us-east-1',
-       
+        userPoolId: 'us-east-1_ZT82sZQs3', // e.g., us-east-1_xxxxx
+        userPoolClientId: '780i8hti5tksrq45s33c8o266m', // e.g., xxxxxxxxxx
+        clientSecret: '15vjtsgsqrumoolif83de1a3kd0ht5el75ufe7ptqo2eu89a97du', // Replace with your actual client secret
+        region: 'us-east-1' // e.g., us-east-1
     },
     api: {
-        invokeUrl: '' // API Gateway endpoint (if any)
+        invokeUrl: '' // e.g., https://xxxxx.execute-api.us-east-1.amazonaws.com/prod
     }
 };
